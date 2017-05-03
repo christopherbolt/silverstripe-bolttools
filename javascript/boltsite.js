cba896636e1f42650a23c45563be0ad27cc57e27
christopherbolt <chris@christopherbolt.com>
2017-05-03 12:03:05 +1200

    Improved touch detection, detecting ability to hover rather than touch since that is what we really need

--- javascript/boltsite.js
+++ javascript/boltsite.js
@@ -45,9 +45,11 @@
 		SELF.touchDevicesTest = function() {
 			if(/KHTML|WebKit/i.test(navigator.userAgent) && ('ontouchstart' in window)) {
 				return true;
-			}else{
-				return false;
 			}
+			if (window.matchMedia && window.matchMedia('(hover:none)').matches) {
+				return true;
+			}
+			return false;
 		}
 		SELF.retinaDevicesTest = function() {
 			return ((window.matchMedia && (window.matchMedia('only screen and (min-resolution: 124dpi), only screen and (min-resolution: 1.3dppx), only screen and (min-resolution: 48.8dpcm)').matches || window.matchMedia('only screen and (-webkit-min-device-pixel-ratio: 1.3), only screen and (-o-min-device-pixel-ratio: 2.6/2), only screen and (min--moz-device-pixel-ratio: 1.3), only screen and (min-device-pixel-ratio: 1.3)').matches)) || (window.devicePixelRatio && window.devicePixelRatio > 1.3));
