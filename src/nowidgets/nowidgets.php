24f819b64bfa2cee98a6801bf97189b7600c9dcd
christopherbolt <chris@christopherbolt.com>
2019-05-07 18:09:43 +1200

    Ensure widgets have a BlogID

--- src/nowidgets/nowidgets.php
+++ src/nowidgets/nowidgets.php
@@ -8,6 +8,17 @@ use SilverStripe\Blog\Model\Blog;
 use SilverStripe\Control\Director;
 
 class Widget extends DataObject {
+    public function __construct($record = null, $isSingleton = false, $model = null) {
+		parent::__construct();
+        $entry = Director::get_current_page();
+        if (is_a($entry, 'BlogPost')) {
+            $this->BlogID = $entry->Parent()->ID;
+        } else if (is_a($entry, 'Blog')) {
+            $this->BlogID = $entry->ID;
+        } else {
+            $this->BlogID = Blog::get()->First()->ID;
+        }
+    }
 	function Blog() {
 		if (empty($this->BlogID)) {
 			$entry = Director::get_current_page();
