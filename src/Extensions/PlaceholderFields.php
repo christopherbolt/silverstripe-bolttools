f316029f536785447b8c4ec48fc98e865039f75b
christopherbolt <chris@christopherbolt.com>
2021-11-03 11:47:12 +1300

    Misc updates

--- src/Extensions/PlaceholderFields.php
+++ src/Extensions/PlaceholderFields.php
@@ -5,10 +5,13 @@ namespace ChristopherBolt\BoltTools\Extensions;
 use SilverStripe\Core\Extension;
 use SilverStripe\Core\Config\Config;
 use SilverStripe\View\SSViewer;
+use SilverStripe\Admin\LeftAndMain;
+use SilverStripe\Control\Controller;
 
 class PlaceholderFields extends Extension {
     // Add the placeholder attribute to supported form fields
     function updateAttributes(&$attributes) {
+        if (is_a(Controller::curr(), LeftAndMain::class)) return;
         $placeholders_supported = $this->owner->config()->get('placeholders_supported');
         if (!is_array($placeholders_supported)) $placeholders_supported = [];
         if (($title = $this->owner->Title()) && !isset($attributes['placeholder']) && in_array(get_class($this->owner), $placeholders_supported) && !$this->owner->hasClass('hasLabel') && !$this->owner->hasClass('has-label')) {
